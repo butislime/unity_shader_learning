06ace8ccb601d21b69867d74013044ca238dfc1f
buti_s <butislime@gmail.com>
2019-01-15 04:07:23 +0900

    add projection shadow test

---
+++ Assets/Script/ShadowMapCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowMapCamera : MonoBehaviour
+{
+	public RenderTexture shadowMap { get; private set; }
+	public new Camera camera { get; private set; }
+
+	readonly int projectionShadowMapID = Shader.PropertyToID("_ProjectionShadowMap");
+	readonly int projectionViewProjID = Shader.PropertyToID("_ProjectionViewProj");
+	void Awake()
+	{
+		camera = GetComponent<Camera>();
+
+		shadowMap = RenderTexture.GetTemporary(256, 256, 16, RenderTextureFormat.ARGB32);
+		Shader.SetGlobalTexture(projectionShadowMapID, shadowMap);
+		camera.targetTexture = shadowMap;
+	}
+
+	void OnPreRender()
+	{
+		Matrix4x4 view = camera.worldToCameraMatrix;
+		Matrix4x4 proj = GL.GetGPUProjectionMatrix(camera.projectionMatrix, true);
+		Shader.SetGlobalMatrix(projectionViewProjID, proj * view);
+	}
+}
---
+++ Assets/Script/ShadowMapView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShadowMapView : MonoBehaviour
+{
+	[SerializeField] RawImage shadowMapImage;
+	[SerializeField] Camera shadowMapCamera;
+
+	void Start()
+	{
+		shadowMapImage.texture = shadowMapCamera.targetTexture;
+	}
+}
